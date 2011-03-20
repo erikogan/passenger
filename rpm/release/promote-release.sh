9fa9de7caf8026cb1328c33c40dd9aa91a920ff4
Erik Ogan <erik@ogan.net>
2011-03-20 14:43:49 -0700

    Simple script for my convenience.

---
+++ rpm/release/promote-release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+staging=`readlink STAGING`
+live=`readlink LIVE`
+
+pushd STAGING
+
+for dir in {fedora/1{4,3},rhel/5}/{x86_64,i386}
+do
+  echo $dir -------------
+  createrepo $PWD/$dir
+done
+
+popd
+
+# Swap them (removing the old link prevents creating a link w/in a link)
+rm -f LIVE STAGING
+ln -sf $staging LIVE
+ln -sf $live STAGING
+
+# Backport
+rsync -av --progress LIVE/. STAGING/.
